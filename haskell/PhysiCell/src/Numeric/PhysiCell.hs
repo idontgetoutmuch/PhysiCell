6084bb81cf2ecea9e225ee3c83b7b12f099d89a5
Dominic Steinitz <dominic@steinitz.org>
2020-10-21 11:38:07 +0100

    Start of Hakell bindings to PhysiCell

---
+++ haskell/PhysiCell/src/Numeric/PhysiCell.hs
@@ -0,0 +1,4 @@
+module Numeric.PhysiCell where
+
+runPhysiCell :: Double -> Double
+runPhysiCell x = x
---
+++ shell.nix
@@ -0,0 +1,26 @@
+let
+
+nixpkgs = builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/20.03.tar.gz";
+    sha256 = "0182ys095dfx02vl2a20j1hz92dx3mfgz2a6fhn31bqlp1wa8hlq";
+};
+
+in
+
+with import nixpkgs {};
+
+let
+
+openmp = llvmPackages.openmp;
+
+in
+
+mkShell {
+  buildInputs = [
+    cabal-install
+    # gcc may not be necessary
+    gcc
+    ghc
+    openmp
+  ];
+}
